90fd9bcded0a08eaa059abbb197254be965af411
Caesarzuo <125637293+Caesarzuo@users.noreply.github.com>
2023-10-07 15:43:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oJrLg6nsEnOc7RJM-V6RLqeI4V0A',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'yiK8cJnrrotgCEpx-d0AZfdIxyLJzYZQ49G3A6VC2vs',
+      useTemplateId: 'cjPGZj9cLiOyHDgxROLIwt7FbBnPhBt_BntXeXkYww8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-28',
       festivals: [
